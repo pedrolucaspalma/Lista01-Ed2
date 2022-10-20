d02bbe75a996a88476f8e0b8c3ec4bc62fb09f35
Pedro Palma <pedrolucaspalma@gmail.com>
2022-10-19 23:23:46 -0300

    Add C# and python

---
+++ Csharp.cs
@@ -0,0 +1,13 @@
+using System;
+public class GFG {
+ 
+    public static void Main(String[] args)
+    {
+        int i, n = 8;
+        for (i = 1; i <= n; i++) {
+            Console.Write("Estruturas de Dados 2\n");
+        }
+    }
+}
+
+// “Estruturas de Dados 2” é impresso apenas n vezes na tela, pq o valor de n pode mudar.Por isso a complexidade do tempo é linear: O(n), ou seja, toda vez, uma quantidade linear de tempo é necessária para executar o código.
---
+++ python.py
@@ -0,0 +1,5 @@
+n = 4
+for i in range(1, n + 1):
+	print("Estrutura de Dados")
+  
+# A complexidade é O(n), linear, pois "Estrutura de Dados" é impresso n vezes na tela, pois o valor de n pode mudar. Ou seja, toda vez uma quantidade linear é necessária para executar o código
